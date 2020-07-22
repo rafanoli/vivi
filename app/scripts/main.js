ae7af561def151902e85a97f52c360ba3dbf2778
Rafael Andrade <rafaelandrade@wizsolucoes.com.br>
2020-07-21 23:31:38 -0300

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+    $('.tilt-content').tilt({
+        maxTilt: 15,
+        perspective: 1400,
+        easing: 'cubic-bezier(.03,.98,.52,.99)',
+        speed: 1200,
+        glare: true,
+        maxGlare: 0.2,
+        scale: 1.2
+    });
+
+}(jQuery));
